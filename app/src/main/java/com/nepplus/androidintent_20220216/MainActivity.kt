d2e7e3676f5bb51958d57387836f95aa71d872dd
이 영 종 <naki5087@naver.com>
2022-02-16 11:03:35 +0900

    전화 걸기 (DIAL) 코틀린 코드 작성

--- app/src/main/java/com/nepplus/androidintent_20220216/MainActivity.kt
+++ app/src/main/java/com/nepplus/androidintent_20220216/MainActivity.kt
@@ -1,11 +1,23 @@
 package com.nepplus.androidintent_20220216
 
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btnDial.setOnClickListener {
+            val inputPhoneNum = edtPhoneNum.text.toString()
+//            Intent에, 어디로 전화를 걸지 알려줘야함. Uri를 이용해서 알려주자.
+            val myUri = Uri.parse("tel:${inputPhoneNum}") // 띄어쓰기는 끼면 안됨
+            val myIntent = Intent(Intent.ACTION_DIAL, myUri)
+
+            startActivity(myIntent)
+        }
     }
 }
