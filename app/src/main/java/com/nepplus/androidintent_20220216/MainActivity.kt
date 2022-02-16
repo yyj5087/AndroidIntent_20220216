886cb89343ddc6fad8215a185542b6da170981c7
이 영 종 <naki5087@naver.com>
2022-02-16 11:47:47 +0900

    플레이 스토어로 바로 링크

--- app/src/main/java/com/nepplus/androidintent_20220216/MainActivity.kt
+++ app/src/main/java/com/nepplus/androidintent_20220216/MainActivity.kt
@@ -40,7 +40,12 @@ class MainActivity : AppCompatActivity() {
         }
 
         btnNaverWebLink.setOnClickListener {
-            val myUri = Uri.parse("naver.com")
+            val myUri = Uri.parse("https://naver.com")
+            val myIntent =Intent(Intent.ACTION_VIEW, myUri)
+            startActivity(myIntent)
+        }
+        btnKakaoTalkPlayStoreLink.setOnClickListener {
+            val myUri = Uri.parse("market://details?id=com.kakao.talk")
             val myIntent =Intent(Intent.ACTION_VIEW, myUri)
             startActivity(myIntent)
         }
